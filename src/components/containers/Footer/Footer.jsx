4fb30f906d4bdec7259d8f2c13d9dd38ac538e93
Daniil Kaminskyi <98901710+whit33th@users.noreply.github.com>
2024-10-25 23:50:13 +0200

    1.03

--- src/components/containers/Footer/Footer.jsx
+++ src/components/containers/Footer/Footer.jsx
@@ -1,15 +1,15 @@
 import styles from "./Footer.module.scss";
-import checked from "asset/ico/check.png"
-import inst from 'asset/ico/instagram.svg'
-import fb from 'asset/ico/facebook.svg'
-import x from 'asset/ico/x.svg'
-import linkedin from 'asset/ico/linkedin.png'
+import checked from "asset/ico/check.png";
+import inst from "asset/ico/instagram.svg";
+import fb from "asset/ico/facebook.svg";
+import x from "asset/ico/x.svg";
+import linkedin from "asset/ico/linkedin.png";
 function Footer() {
   return (
     <div className={styles.footer}>
       <div className={styles.topLayer}>
         <div className={styles.info}>
-          <p style={{fontWeight: '300'}}>Sync Design</p>
+          <p style={{ fontWeight: "300" }}>Sync Design</p>
           <h2>
             We elevate your brand <br /> with a dedicated design expert
           </h2>
@@ -17,33 +17,53 @@ function Footer() {
         </div>
         <div>
           <p>Company</p>
-          <a className="sText">Blog</a>
-          <a className="sText">Careers</a>
-          <a className="sText">Pricing</a>
+          <a href="#" className="sText">
+            Blog
+          </a>
+          <a href="#" className="sText">
+            Careers
+          </a>
+          <a href="#" className="sText">
+            Pricing
+          </a>
         </div>
         <div>
           <p>Resources</p>
-          <a href='#' className="sText">Documentation </a>
-          <a href='#' className="sText">Papers</a>
-          <a href='#' className="sText">Press Conferences </a>
+          <a href="#" className="sText">
+            Documentation{" "}
+          </a>
+          <a href="#" className="sText">
+            Papers
+          </a>
+          <a href="#" className="sText">
+            Press Conferences{" "}
+          </a>
         </div>
         <div>
           <p>Legal </p>
-          <a href='#' className="sText">Terms of Service</a>
-          <a href='#' className="sText">Privacy Policy</a>
-          <a href='#' className="sText">Cookies Policy</a>
-          <a href='#' className="sText">Data Processing</a>
+          <a href="#" className="sText">
+            Terms of Service
+          </a>
+          <a href="#" className="sText">
+            Privacy Policy
+          </a>
+          <a href="#" className="sText">
+            Cookies Policy
+          </a>
+          <a href="#" className="sText">
+            Data Processing
+          </a>
         </div>
       </div>
       <hr />
-      <div >
+      <div>
         <div className={styles.botInfo}>
-          <img  src={checked} alt="" loading="lazy" />
-          <p > Active in all social networks</p>
+          <img src={checked} alt="" loading="lazy" />
+          <p> Active in all social networks</p>
         </div>
         <div className={styles.socialLinks}>
           <button>
-            <img src={inst} alt=""  loading="lazy"/>
+            <img src={inst} alt="" loading="lazy" />
             <span>Instagram</span>
           </button>
           <button>
